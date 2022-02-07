3bd91b70162a6e457b00fc3c450b711770c9bbf1
Ellie Redmayne <ellieredmayne@MacBook-Air-2.lan>
2022-02-07 11:15:17 +0000

    animations, scroll to top

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
-console.log("hello world");
+//Fade animation
+AOS.init({duration: 2000});
 
+//Typing Animation
+const title = "Ellie Redmayne"; 
+const subtitle ="Web Developer ";
+const titleArray = title.split("");
+const subtitleArray = subtitle.split("");
+let timer;
+
+function frameLooper () {
+	if (titleArray.length > 0 || subtitleArray.length > 0) {
+		document.getElementById("title-text").innerHTML += titleArray.shift()
+    document.getElementById("subtitle-text").innerHTML += subtitleArray.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',100); 
+
+}
+frameLooper();
+
+//Nav bar 
 const navToggle = document.querySelector(".nav-toggle");
 
 const navLinks = document.querySelectorAll(".nav__link");
@@ -13,3 +34,21 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+//Scroll to top
+scrollButton = document.getElementById("scroll-top");
+
+window.onscroll = function() {scrollDisplay()};
+
+function scrollDisplay() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function handleTopScroll() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
